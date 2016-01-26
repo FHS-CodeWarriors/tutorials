04ad590320d694e702669556766bc4c161b69625
Jacob Joaquin <jacobjoaquin@gmail.com>
2016-01-26 15:39:43 -0800

    Cleaned up array example

--- examples/array/sketch.js
+++ examples/array/sketch.js
@@ -1,18 +1,16 @@
-var myArray = [10, 10, 20, 30, 50, 80, 130];
+var diameters = [10, 10, 20, 30, 50, 80, 130];
 
 function setup() {
     createCanvas(500, 500);
     noLoop();
-    stroke(0);
 }
 
 function draw() {
     background(180);
-    
-    var length = myArray.length;
+
+    var length = diameters.length;
     for (var i = 0; i < length; i++) {
-        var diameter = myArray[i];
-        print(diameter);
-        ellipse(i / (length - 1) * width, 250, diameter, diameter);
+        var d = diameters[i];
+        ellipse(i / (length - 1) * width, 250, d, d);
     }
 }
