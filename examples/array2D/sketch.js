82890a85698317d5767ceebe12790456665b6dde
Jacob Joaquin <jacobjoaquin@gmail.com>
2016-01-26 20:46:54 -0800

    Cleaned example

--- examples/array2D/sketch.js
+++ examples/array2D/sketch.js
@@ -1,4 +1,17 @@
-var positions = [[50, 50], [50, 450], [250, 250], [450, 50], [450, 450]];
+/*
+A mulit-dimensional is an array that stores other arrays or arrays
+of arrays. In this example, x-y coordinate pairs are organized as
+2-element arrays. The 2-element array pairs are stored inside an
+array, forming a 2D array.
+*/
+
+var positions = [
+    [50, 50],
+    [50, 450],
+    [250, 250],
+    [450, 50],
+    [450, 450]
+];
 
 function setup() {
     createCanvas(500, 500);
